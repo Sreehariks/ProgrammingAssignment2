4fb2029286fcff297cda3f2bf6ede8e843fbb6c7
Sreehariks <sreehariks@gmail.com>
2014-11-23 14:02:15 +0530

    Edited version of cachematrix.R
    code changes to implement inverse of matrix calculation and use the
    cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions makeCacheMatrix() and cacheSolve() are used 
+## to implement to calculate invers of a matrix, but take advantage of the cache
+## if it has already been calculated
 
-## Write a short comment describing this function
+## makeCacheMatrix Function 
+## @x: a square invertible matrix
+## return: a list containing functions to
+##              1. set the matrix
+##              2. get the matrix
+##              3. set the inverse
+##              4. get the inverse
+##         this list is used as the input to cacheSolve()
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
+   
+  invMat = NULL
+  
+  
+  set = function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
+    x <<- y
+    invMat <<- NULL
+  }
+  
+  get = function() x
+  setinv = function(inverse) invMat <<- inverse 
+  getinv = function() invMat
+  list(set=set, get=get, 
+       setinv=setinv, 
+       getinv=getinv)
+  
+  
 }
 
 
-## Write a short comment describing this function
+## @x: output of makeCacheMatrix()
+## return: inverse of the original matrix input to makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invMat = x$getinv()
+  
+  # Asscertain if the inverse has been calculated earier 
+  
+  if (!is.null(invMat)){
+    message("Cache Check :  Using Cached Data")
+    return(invMat)
+  }
+  
+  mat.data = x$get()
+  
+  # solve function call - first argument is the matrix ; second arg is missing to get an inverse
+  
+  invMat = solve(mat.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  
+  x$setinv(invMat)
+  
+  return(invMat)
+  
 }
